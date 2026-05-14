ca730c638875252dcf1daf7b832011b867ab339f
OwOchiii <nguyenvuan2005@gmail.com>
2026-05-14 17:07:51 +0700

    Update TodoRepository.java

--- src/main/java/orochi/todo_/repository/TodoRepository.java
+++ src/main/java/orochi/todo_/repository/TodoRepository.java
@@ -3,8 +3,11 @@ package orochi.todo_.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import orochi.todo_.model.Todo;
 
+import java.util.List;
+
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
     Todo save(Todo todo);
     void deleteById(Integer id);
-
+    Todo getTodosById(Integer id);
+    List<Todo> findAllByUser_UserId(Integer userId);
 }
